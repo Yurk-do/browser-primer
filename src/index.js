cbd71428e47c7b97ca1663d053c6143845bd677d
Yurk-do <64039182+Yurk-do@users.noreply.github.com>
2020-10-23 17:12:49 +0300

    homework(flatten)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+/* eslint-disable no-return-assign */
+/* eslint-disable import/prefer-default-export */
+/* eslint-disable no-console */
+
 const browserTree = {
   label: "Window",
   children: [
@@ -67,8 +71,10 @@ const browserTree = {
   ],
 };
 
+// рекурсия
+
 const nodesHierarchy = {
-  label: "Event Target",
+  label: "EventTarget",
   children: [
     {
       label: "Node",
@@ -112,35 +118,123 @@ const nodesHierarchy = {
   ],
 };
 
-function renderTree(tree) {
-  const rootUlElement = document.createElement("ul");
-  (function render() {
-    const { label, children } = tree;
-    const childUl = document.createElement("ul");
-    const rootLiElement = document.createElement("li");
-    rootLiElement.innerHTML = label;
-    rootUlElement.appendChild(rootLiElement);
-    rootUlElement.appendChild(childUl);
-    if (children.length > 0) {
-      for (let i = 0; i < children.length; i += 1) {
-        childUl.appendChild(rootLiElement);
-        tree = children[i];
-        render();
-      }
-    }
+const data = [
+  {
+    label: "data 001",
+    level: "001",
+    children: [],
+  },
+  {
+    label: "data 002",
+    level: "002",
+    children: [],
+  },
+  {
+    label: "data 003",
+    level: "003",
+    children: [],
+  },
+  {
+    label: "data 004",
+    level: "004",
+    children: [],
+  },
+];
 
-    return rootUlElement;
-  })();
+function createLiElement(text) {
+  const li = document.createElement("li");
+  li.innerHTML = text;
+  return li;
+}
+
+function renderTreeToList(treeArray) {
+  const list = document.createElement("ol");
+
+  treeArray
+    .map((node) => `${node.label} ${node.level}`)
+    .map((item) => createLiElement(item))
+    .forEach((liElement) => list.append(liElement));
+
+  return list;
+}
+
+function renderTreeToList2(treeArray) {
+  const list = document.createElement("ul");
+
+  treeArray
+    .map((node) => `${node}`)
+    .map((item) => createLiElement(item))
+    .forEach((liElement) => list.append(liElement));
+
+  return list;
+}
+
+function renderTree(treeArray) {
+  if (treeArray.length === 0) return null;
+
+  const rootUlElement = document.createElement("ul");
+
+  treeArray.forEach((node) => {
+    const liElement = document.createElement("li");
+    liElement.innerHTML = node.label;
+    rootUlElement.append(liElement);
+
+    const subTree = renderTree(node.children);
+    if (subTree !== null) rootUlElement.append(subTree);
+  });
+
   return rootUlElement;
 }
 
+const complexArray = [1, 2, 3, [4, 5, 6, [7, 8, 9], 10, [11, 12]]];
+
+function flattenArray(array) {
+  let result = [];
+  array.forEach((item) => {
+    if (Array.isArray(item)) {
+      result = [...result, ...flattenArray(item)];
+    } else {
+      result = [...result, item];
+    }
+  });
+
+  return result;
+}
+
+function flattenTree(tree) {
+  let result = [];
+  const { label, children } = tree;
+  result.push(label);
+  if (children.length > 0) {
+    for (let i = 0; i < children.length; i += 1) {
+      result.push(flattenTree(children[i]));
+    }
+  }
+  return result;
+}
+
+function renderArray(array) {
+  const p = document.createElement("p");
+  p.innerHTML = JSON.stringify(array);
+
+  return p;
+}
+
 export function renderPage() {
-  const browserTreeList = renderTree(browserTree);
-  const nodesHierarchyList = renderTree(nodesHierarchy);
+  const tree = renderTree([browserTree, nodesHierarchy]);
+  const list = renderTreeToList(data);
 
   const rootDiv = document.getElementById("root");
-  rootDiv.append(browserTreeList);
-  rootDiv.append(nodesHierarchyList);
+  if (tree !== null) rootDiv.append(tree);
+  if (list !== null) rootDiv.append(list);
+
+  const flatArray = flattenArray(complexArray);
+  rootDiv.append(renderArray(flatArray));
+
+  const flatTree = flattenTree(browserTree);
+  rootDiv.append(renderArray(flatTree));
+  const list2 = renderTreeToList2(flatTree);
+  if (list2 !== null) rootDiv.append(list2);
 }
 
 export function buildPage() {
@@ -209,4 +303,17 @@ export function buildPage() {
   rootDiv.append(clone);
 
   // rootDiv.remove();
+
+  const demo001 = document.getElementById("demo001");
+
+  console.log(demo001.classList);
+
+  demo001.classList.add("demo-cls");
+
+  demo001.style.position = "relative";
+
+  console.log(demo001.style.marginTop);
+
+  const computedStyle = getComputedStyle(demo001);
+  console.log(computedStyle);
 }
